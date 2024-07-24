97377a06ac29284df6ded043f54ba8d6dafa716c
Jair0305 <chamaco_03@hotmail.es>
2024-07-23 20:02:44 -0600

    .

--- src/main/java/com/back/controller/LoginController.java
+++ src/main/java/com/back/controller/LoginController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ public class LoginController {
             description = "Authenticate a user and return the authentication token along with user details."
     )
     @PostMapping("/login")
+    @PreAuthorize("permitAll()")
     public ResponseEntity<?> login(@RequestBody JwtRequest request) throws Exception {
         try {
             String name = request.getUsername();
