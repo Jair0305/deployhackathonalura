227a3bed05493d072952cdafc25472c3fae34854
Jair0305 <chamaco_03@hotmail.es>
2024-07-23 18:15:49 -0600

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Iniciar Keycloak en segundo plano
+# Inicia Keycloak en segundo plano
 /opt/keycloak/bin/kc.sh start-dev &
 
-# Esperar unos segundos para asegurarse de que Keycloak se inicia correctamente
-sleep 20
+# Espera unos segundos para asegurar que Keycloak se inicie completamente
+sleep 30
 
-# Iniciar la aplicación Spring Boot
+# Inicia la aplicación Spring Boot
 java -jar /app/app.jar
